a62f7540ced662d2c22d44104fe206cd2a19fd8f
Evan <evanjfrancis@gmail.com>
2017-06-01 19:48:12 -0700

    unit tests for mutations

--- src/imports/modules/counter/client/state/counter-actions-mutations.test.js
+++ src/imports/modules/counter/client/state/counter-actions-mutations.test.js
@@ -1,32 +1,59 @@
 import { actions, mutations, MUTATION_TYPES } from './counter-actions-mutations';
 
 describe('counter-actions-mutations', () => {
-  describe('increment', () => {
-    it('commits INCREMENT with amount payload', () => {
-      const commit = jest.fn();
-      const amount = 5;
-      actions.increment({ commit }, { amount });
-      expect(commit.mock.calls.length).toEqual(1);
-      expect(commit.mock.calls[0][0]).toEqual(MUTATION_TYPES.INCREMENT);
-      expect(commit.mock.calls[0][1]).toEqual({ amount });
+  describe('actions', () => {
+    describe('increment', () => {
+      it('commits INCREMENT with amount payload', () => {
+        const commit = jest.fn();
+        const amount = 5;
+        actions.increment({ commit }, { amount });
+        expect(commit.mock.calls.length).toEqual(1);
+        expect(commit.mock.calls[0][0]).toEqual(MUTATION_TYPES.INCREMENT);
+        expect(commit.mock.calls[0][1]).toEqual({ amount });
+      });
+    });
+    describe('decrement', () => {
+      it('commits DECREMENT with amount payload', () => {
+        const commit = jest.fn();
+        const amount = 5;
+        actions.decrement({ commit }, { amount });
+        expect(commit.mock.calls.length).toEqual(1);
+        expect(commit.mock.calls[0][0]).toEqual(MUTATION_TYPES.DECREMENT);
+        expect(commit.mock.calls[0][1]).toEqual({ amount });
+      });
+    });
+    describe('resetDelayed', () => {
+      it('commits RESET after delay', async () => {
+        const commit = jest.fn();
+        await actions.resetDelayed({ commit });
+        expect(commit.mock.calls.length).toEqual(1);
+        expect(commit.mock.calls[0][0]).toEqual(MUTATION_TYPES.RESET);
+      });
     });
   });
-  describe('decrement', () => {
-    it('commits DECREMENT with amount payload', () => {
-      const commit = jest.fn();
-      const amount = 5;
-      actions.decrement({ commit }, { amount });
-      expect(commit.mock.calls.length).toEqual(1);
-      expect(commit.mock.calls[0][0]).toEqual(MUTATION_TYPES.DECREMENT);
-      expect(commit.mock.calls[0][1]).toEqual({ amount });
+  describe('mutations', () => {
+    describe(MUTATION_TYPES.INCREMENT, () => {
+      it('adds amount to count', () => {
+        const state = { count: 0 };
+        const amount = 5;
+        mutations[MUTATION_TYPES.INCREMENT](state, { amount });
+        expect(state.count).toEqual(5);
+      });
     });
-  });
-  describe('resetDelayed', () => {
-    it('commits RESET after delay', async () => {
-      const commit = jest.fn();
-      await actions.resetDelayed({ commit });
-      expect(commit.mock.calls.length).toEqual(1);
-      expect(commit.mock.calls[0][0]).toEqual(MUTATION_TYPES.RESET);
+    describe(MUTATION_TYPES.DECREMENT, () => {
+      it('adds amount to count', () => {
+        const state = { count: 0 };
+        const amount = 5;
+        mutations[MUTATION_TYPES.DECREMENT](state, { amount });
+        expect(state.count).toEqual(-5);
+      });
+    });
+    describe(MUTATION_TYPES.RESET, () => {
+      it('adds amount to count', () => {
+        const state = { count: 10 };
+        mutations[MUTATION_TYPES.RESET](state);
+        expect(state.count).toEqual(0);
+      });
     });
   });
 });
