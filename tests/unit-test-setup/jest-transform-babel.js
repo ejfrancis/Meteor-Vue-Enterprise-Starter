2ace034a3c6f6a5ca1edad4f69d5282d7c9c7d04
Evan <evanjfrancis@gmail.com>
2017-06-06 21:10:08 -0700

    lint

--- tests/unit-test-setup/jest-transform-babel.js
+++ tests/unit-test-setup/jest-transform-babel.js
@@ -1,5 +1,5 @@
 /*
- * Jest unit tests requires some babel configuration since unit tests are using es2015/es2017 but Meteor's node 
+ * Jest unit tests requires some babel configuration since unit tests are using es2015/es2017 but Meteor's node
  * doesn't support them (for now). Putting babel config here so it doesn't have to go in .babelrc and
  * affect Meteor's build system unnecessarily.
  */
