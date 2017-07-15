f6fa3fad156015ce1923226a076012a8e583aac6
Evan Francis <evanjfrancis@gmail.com>
2017-07-15 13:40:30 -0700

    Revert "Test/enable jest vue meteor 1.6a2"

--- src/imports/modules/store/client/lib/store.js
+++ src/imports/modules/store/client/lib/store.js
@@ -3,14 +3,11 @@ import createLogger from 'vuex/dist/logger';
 
 import { counterStoreModule } from './../../../counter/client/state/counter-store.js';
 
-const plugins = [];
-if (['production', 'test'].indexOf(process.env.NODE_ENV) === -1) {
-  plugins.push(createLogger());
-}
-
 const createStore = () => {
   const store = new Store({
-    plugins,
+    plugins: [
+      createLogger()
+    ],
     modules: {
       counter: counterStoreModule
     }
--- tests/unit-test-setup/meteor-mocks/meteor.js
+++
@@ -1,4 +0,0 @@
-export const Meteor = {
-  call: () => null,
-  startup: () => null
-};
