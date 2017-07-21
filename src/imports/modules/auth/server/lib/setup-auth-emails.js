42744fa1872e2f7418a1481eca1f8a1271eb14e4
Evan <evanjfrancis@gmail.com>
2017-07-20 19:08:46 -0700

    fix remove hash in password reset email

--- src/imports/modules/auth/server/lib/setup-auth-emails.js
+++ src/imports/modules/auth/server/lib/setup-auth-emails.js
@@ -6,7 +6,9 @@ const setUpAuthEmails = () => {
   Accounts.emailTemplates.resetPassword.subject = () => `Password reset for ${Meteor.settings.public.siteName}`;
   Accounts.emailTemplates.resetPassword.text = (user, url) => `Hello ${user.username},
 
-A password reset has been requested for your account on ${Meteor.settings.public.siteName}. To continue resetting your password please click the following link: ${url.replace('#', '')}
+A password reset has been requested for your account on ${Meteor.settings.public.siteName}. To continue resetting your password please click the following link:
+
+${url.replace('#/', '')}
 
 If you didn't request to change your password, please ignore this email. If it keeps happening, please contact us.
 
