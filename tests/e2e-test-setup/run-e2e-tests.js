bf0c1fd557ed01a3c9d13d372aed3a28ed2771bb
Evan <evanjfrancis@gmail.com>
2017-08-06 20:51:07 -0700

    Remove git-all install and try increasing chimp process max buffer

--- tests/e2e-test-setup/run-e2e-tests.js
+++ tests/e2e-test-setup/run-e2e-tests.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process');
 const meteorCommand = process.argv[2];
 const chimpCommand = process.argv[3];
 
-const meteorProcess = exec(meteorCommand); // 'npm start'
+const meteorProcess = exec(meteorCommand); // ex: 'meteor npm run start'
 
 meteorProcess.stdout.pipe(process.stdout);
 meteorProcess.stderr.pipe(process.stderr);
@@ -12,7 +12,7 @@ meteorProcess.stderr.pipe(process.stderr);
 // Wait until Meteor is started and the start the chimp tests
 meteorProcess.stdout.on('data', (data) => {
   if (data.toString().match('App running at')) {
-    const chimpProcess = exec(chimpCommand); // 'npm run test:chimp:phantomjs'
+    const chimpProcess = exec(chimpCommand, { maxBuffer: 20480000 }); // 'ex: meteor npm run test'
 
     chimpProcess.stdout.pipe(process.stdout);
     chimpProcess.stderr.pipe(process.stderr);
