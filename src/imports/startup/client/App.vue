5b854ed38a455c60e817daed7abf99a4f22e8dbc
Evan <evanjfrancis@gmail.com>
2017-08-13 22:35:08 -0700

    square borders for iView forms

--- src/imports/startup/client/App.vue
+++ src/imports/startup/client/App.vue
@@ -1,65 +1,14 @@
 <style lang="scss">
-/* global app styles */
-// body {
-//   font-family: Helvetica, Arial, sans-serif;
-//   font-size: 13px;
-
-//   h1, h2, h3, h4, h5 {
-//     margin: 10px 0px;
-//   }
-//   h1 {
-//     font-size: 2.4em;
-//   }
-//   h2 {
-//   font-size: 2.0em;
-//   }
-//   h3 {
-//     font-size: 1.6em;
-//   }
-//   h4 {
-//     font-size: 1.4em;
-//   }
-//   h5 {
-//     font-size: 1.2em;
-//   }
-// }
-// html {
-//   height: 100%;
-// }
-
-// em {
-//   font-style: italic;
-// }
-// strong {
-//   font-weight: 600;
-// }
-// p {
-//   padding: 5px 0px;  
-// }
-// input, button {
-//   padding: 5px 5px;
-// }
-
-// button, a, p, label, input, strong, em {
-//   font-size: 13px !important;
-// }
-// @media (max-width: 768px) {
-//   html, body, button, a, p, label, input, strong, em, .ivu-btn, .ivu-menu-icon {
-//     font-size: 1.1em !important;
-//   }
-// }
-// @media (min-width: 768px) {
-//   html, button, a, p, label, input, strong, em, .ivu-btn, .ivu-menu-icon {
-//     font-size: 1em !important;
-//   }
-// }
-
 /* iView framework overrides */
 .ivu-btn {
   border-radius: 0;
 }
+// hide * before required fields
 .ivu-form-item-required .ivu-form-item-label::before {
   display: none;
+}
+.ivu-input, .ivu-input-group-append, .ivu-input-group-prepend {
+  border-radius: 0;
 }
 /* end iView framework override */
 
