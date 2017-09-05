870c43a050c404359779ab5eb979483930b6c60b
Evan <evanjfrancis@gmail.com>
2017-09-04 23:11:17 -0700

    only allow logged in to check users role

--- src/imports/modules/accounts/shared/methods/check-user-global-role.js
+++ src/imports/modules/accounts/shared/methods/check-user-global-role.js
@@ -14,6 +14,9 @@ const checkUserGlobalRole = new ValidatedMethod({
   run ({ roles }) {
     // only run on server
     if (!this.isSimulation) {
+      if (!Meteor.userId()) {
+        return false;
+      }
       return Roles.userIsInRole(Meteor.userId(), roles, Roles.GLOBAL_Group);
     }
   }
