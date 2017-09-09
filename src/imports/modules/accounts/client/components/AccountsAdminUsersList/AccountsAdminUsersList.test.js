7d9f41044abcebb3b1a55ff772bf91ea6a4a661b
Evan <evanjfrancis@gmail.com>
2017-09-09 14:10:56 -0700

    fix test

--- src/imports/modules/accounts/client/components/AccountsAdminUsersList/AccountsAdminUsersList.test.js
+++ src/imports/modules/accounts/client/components/AccountsAdminUsersList/AccountsAdminUsersList.test.js
@@ -1,9 +1,15 @@
 import { mount } from 'avoriaz';
 import AccountsAdminUsersList from './AccountsAdminUsersList.vue';
+import { setupVue } from '/src/imports/startup/client/client-index';
 
 describe('AccountsAdminUsersList', () => {
+  let store;
+
+  beforeEach(() => {
+    store = setupVue().store;
+  });
   it('renders without crashing', () => {
-    const wrapper = mount(AccountsAdminUsersList);
+    const wrapper = mount(AccountsAdminUsersList, { store });
     expect(wrapper.find('.AccountsAdminUsersList').length).toEqual(1);
   });
 });
